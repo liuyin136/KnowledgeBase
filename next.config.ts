d87d7f9de5740fa6f6c99f94f0ede81625b526a0
liuyin136 <yinliu136@gmail.com>
2026-07-05 00:43:16 +0800

    chore: update tw-animate-css to latest

--- next.config.ts
+++ next.config.ts
@@ -2,11 +2,17 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "standalone",
-  /* config options here */
   typescript: {
     ignoreBuildErrors: true,
   },
   reactStrictMode: false,
+
+  // 用這個方式強制設定 @/* alias（不依賴 tsconfig 的 baseUrl）
+  turbopack: {
+    resolveAlias: {
+      "@/*": "./src/*",
+    },
+  },
 };
 
 export default nextConfig;
